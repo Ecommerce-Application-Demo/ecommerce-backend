ab4b5adb289b83dc353f4e2f8022d6ccdf063bc8
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-01-27 00:52:08 +0530

    added CORS config

--- CustomerService/src/main/java/com/ecommerce/customer/Config.java
+++ CustomerService/src/main/java/com/ecommerce/customer/Config.java
@@ -25,7 +25,7 @@ public class Config {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("*")
+				registry.addMapping("/**")
 						.allowedOrigins("http://localhost:3000");
 			}
 		};
