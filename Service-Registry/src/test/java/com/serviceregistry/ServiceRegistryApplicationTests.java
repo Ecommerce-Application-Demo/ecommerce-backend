e1747eb9f58489067f92c5cd20490af61542b096
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-02-02 13:47:50 +0530

    Service Registry added

---
+++ Service-Registry/src/test/java/com/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
